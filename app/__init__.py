71eed5e8175a27c65b3c18a836bd144014f6e5fd
Dimas Aziz Kurniawan <suprafit9301@gmail.com>
2024-04-19 17:16:22 +0700

    first commit of passage

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+    from .main import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    
+    return app
